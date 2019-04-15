5a9360804ddb56e90658cdce89bf80846b8eae30
alfonso.herreros <aherreros@atsistemas.com>
2019-04-15 13:36:03 +0200

    vaya

--- src/main/java/com/docomo/fraudwall/configserver/ConfigServerApp.java
+++ src/main/java/com/docomo/fraudwall/configserver/ConfigServerApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApp  {
-
+//Hola
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApp.class, args);
 	}
